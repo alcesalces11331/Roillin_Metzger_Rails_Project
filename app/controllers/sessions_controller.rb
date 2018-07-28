30168172da5a287a5d12472615141b94f713fb9e
alceslalces11331 <rollin.metzer@gmail.com>
2018-07-28 18:14:54 -0500

    Sessions#new&&destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,14 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+
+  end
+
+  def destroy
+    session.delete :user_id
+    redirect_to root_path
+  end
 end
