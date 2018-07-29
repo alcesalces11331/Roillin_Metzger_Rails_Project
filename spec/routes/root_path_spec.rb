90c29a496e326ee233e27a6bf2166494d410a083
alceslalces11331 <rollin.metzer@gmail.com>
2018-07-28 19:02:15 -0500

    root_path specs conventions

---
+++ spec/routes/root_path_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "route for root", :type => :routing do
+  render_views
+  describe 'get welcome' do
+    it 'routes / to static controller' do
+      expect(get("/")).to route_to("static#welcome")
+    end
+  end
+
+end
