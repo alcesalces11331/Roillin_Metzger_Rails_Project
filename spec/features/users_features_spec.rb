417b98f31dd8f3872b200090b034a313285cd3d2
alcesalces11331 <rollin.metzer@gmail.com>
2018-07-30 19:11:09 -0500

    User Signup tests

--- spec/features/users_features_spec.rb
+++ spec/features/users_features_spec.rb
@@ -1,4 +1,19 @@
 require_relative "../rails_helper.rb"
 describe 'Feature Test: User Signup',  :type => :feature do
 
+  it 'successfully signs up as non-admin' do
+    visit '/users/new'
+    expect(current_path).to eq('/users/new')
+    # #user_signup is defined in login_helper.rb
+    user_signup
+    expect(current_path).to eq('/users/1')
+    expect(page).to have_content("Professor McGonagal")
+  end
+
+  it 'on sign up, successfully adds a session hash' do
+    visit '/users/new'
+    user_signup
+    expect(page.get_rack_session_key('user_id')).to_not be_nil
+  end
+
 end
