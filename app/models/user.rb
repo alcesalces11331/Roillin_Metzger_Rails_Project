ff3b541f36509ffba347f6eb628fc0ba4f99eed9
alcesalces11331 <rollin.metzger@gmail.com>
2018-07-31 16:11:14 -0500

    modified to reflect users table additions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :name, :password, presence: true
-  validates :name, uniqueness: true
+  validates :name, :password, :email, presence: true
+  validates :email, uniqueness: true
   validates :name, format: { with: /[^A-Za-z0-9]+/, message: "only allows alpha-numeric characters"}
   #validates :password, format: { with: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/, message: "must contain 1 lowercase alphabetical, 1 uppercase, and 1 number, and have at leaset 8 characters"}
 
