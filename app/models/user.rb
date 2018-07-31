997739b36e7d45914a942100166d92c993af8dd8
alcesalces11331 <rollin.metzger@gmail.com>
2018-07-30 20:19:03 -0500

    User activerecord validations and joins

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  validates :name, :password, presence: true
+  validates :name, uniqueness: true
+  validates :name, format: { with: /[^A-Za-z0-9]+/, message: "only allows alpha-numeric characters"}
+  validates :password, format: { with: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/, message: "must contain 1 lowercase alphabetical, 1 uppercase, and 1 number, and have at leaset 8 characters"}
+
   has_many :cats
   has_many :spells, :through => :cats
+
+  def self.find_or_create_by_omniauth(auth_hash)
+  end
 end
