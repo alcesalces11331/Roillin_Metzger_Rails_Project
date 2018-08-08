03d596dbc043f0625e96239be6aa62721b12b2d5
alcesalces11331 <rollin.metzger@gmail.com>
2018-08-07 20:01:31 -0500

    create_table :cats

---
+++ db/migrate/20180808005109_create_cats.rb
@@ -0,0 +1,13 @@
+class CreateCats < ActiveRecord::Migration
+  def change
+    create_table :cats do |t|
+      t.string :name
+      t.string :type
+      t.integer :age
+      t.string :user_id
+      t.string :school_id
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+    end
+  end
+end
