68bf23f37363d1f6f6d0912258f9e8195501c145
alcesalces11331 <rollin.metzger@gmail.com>
2018-08-08 19:06:06 -0500

    potions and spells for cats

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -2,13 +2,11 @@ class Cat < ActiveRecord::Base
   belongs_to :user
 #  belongs_to :school
   has_many :spells
+  has_many :potions
 
   validates :name, :type, :age, presence: true
 
-  def cast_spell
-  end
-
-  def prepare_spell
+  def create_potion
   end
 
   def learn_spell
--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -9,8 +9,16 @@ RSpec.describe User, type: :model do
       :age => "12"
     )
   }
-  
+
   it "is valid with a name, password, and email" do
     expect(cat).to be_valid
+  end
+
+  context 'Spellcraft methods:' do
+    it 'learns a spell' do
+    end
+
+    it 'creates a potion' do
+    end
   end
 end
