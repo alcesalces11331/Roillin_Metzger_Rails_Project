6763e13aa796d39889054d8018ae6dc3eca4e6fd
alcesalces11331 <rollin.metzger@gmail.com>
2018-08-22 19:32:03 -0500

    initial framework for cats creating spells

--- app/controllers/spells_controller.rb
+++ app/controllers/spells_controller.rb
@@ -2,12 +2,25 @@ class SpellsController < ApplicationController
   before_action :login_checkpoint, only: [:new, :create, :update, :destroy]
 
   def index
+    if params[:cat_id]
+      @spells = Spell.find(params[:cat_id]).spells
+    else
+      @spells = Spell.all
+    end
   end
 
   def new
+    @spell = Spell.new
   end
 
   def create
+    @spell = Spell.new(spell_params)
+    @spell.cat = params[:cat_id]
+    if @spell.save
+      redirect_to cats_path(@spell)
+    else
+      render :new
+    end
   end
 
   def edit
