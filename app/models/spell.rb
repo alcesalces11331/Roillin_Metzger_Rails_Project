20d4c052bcf926a530c5c5347a21ea45c3048352
alcesalces11331 <rollin.metzger@gmail.com>
2018-08-23 18:53:05 -0500

    spells class scope method .by_cat

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -3,4 +3,8 @@ class Spell < ActiveRecord::Base
   belongs_to :cat
 
   validates :name, :description, :power_type, :power_level, presence: true
+
+  def self.by_cat(cat_id)
+    where(cat: cat_id)
+  end
 end
