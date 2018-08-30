d5fa01019548cd9667b8062de6748baedf11e4bc
alcesalces11331 <rollin.metzger@gmail.com>
2018-08-29 19:14:13 -0500

    Spell validates :power_level by numericality

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -3,6 +3,7 @@ class Spell < ActiveRecord::Base
   belongs_to :cat
 
   validates :name, :description, :power_type, :power_level, presence: true
+  validates :power_level, numericality: { greater_than: 0, less_than_or_equal_to: 100 }
 
   def self.by_cat(cat_id)
     where(cat: cat_id)
