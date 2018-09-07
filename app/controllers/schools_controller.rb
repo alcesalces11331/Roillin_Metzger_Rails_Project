2e2b3f3545226a8e17644db01145f0ac169cae25
alcesalces11331 <rollin.metzger@gmail.com>
2018-09-06 20:57:12 -0500

    added methods and logic to enroll the cat in the school

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -3,6 +3,9 @@ class SchoolsController < ApplicationController
   def show
     @school = School.find_by(name: "Howgarts")
     @cat = Cat.find_by(id: params[:id])
+    if @cat.enrolled?
+      enroll_cat(@cat)
+    end
   end
 
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,8 @@
 class School < ActiveRecord::Base
   has_many :cats
   has_many :spells
-  has_many :potions
+
+  def enroll_cat(cat)
+    cat.enrolled = true
+  end
 end
