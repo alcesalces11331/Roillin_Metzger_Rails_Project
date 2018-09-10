8567ff8e990df92bb16bee74217c50f054d381b6
alcesalces11331 <rollin.metzger@gmail.com>
2018-09-10 18:05:31 -0500

    minor change to Spell power_level numericality validation

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -4,7 +4,11 @@ class Spell < ActiveRecord::Base
   belongs_to :cat
 
   validates :name, :description, :power_level, presence: true
-  validates :power_level, numericality: { greater_than: 0, less_than_or_equal_to: 100 }
+  validates :power_level, numericality: {
+    greater_than: 0,
+    less_than_or_equal_to: 9000, 
+    message: "It can't be over 9000"
+  }
   validates :power_type, length: {
     maximum: 2,
     message: "Power can only be one or two types"
