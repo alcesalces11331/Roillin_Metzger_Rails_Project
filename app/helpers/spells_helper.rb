61579afb56be034ffe903639361c020497d0d6ac
alcesalces11331 <rollin.metzger@gmail.com>
2018-09-11 17:41:41 -0500

    didn't need method in spells_helper

--- app/helpers/spells_helper.rb
+++ app/helpers/spells_helper.rb
@@ -4,13 +4,4 @@ module SpellsHelper
     @types = ['fire', 'water', 'earth', 'lightning', 'ice', 'poison', 'death', 'psychic', 'acid', 'wind']
   end
 
-  def match_power_types(params)
-    params = params.join(",")
-    @matched_types = []
-    power_types.each do |p|
-      @matched_types << p if params.match(p)
-    end
-    @matched_types
-  end
-
 end
